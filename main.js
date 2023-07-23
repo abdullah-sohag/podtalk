1a47a745086c96caae4c917fe514ccb67389225a
abdullah sohag <abdullahsohag74@gmail.com>
2023-07-23 08:27:28 +0600

    latest apesode

---
+++ main.js
@@ -0,0 +1,12 @@
+let splide = new Splide( '.splide', {
+    perPage: 3,
+    rewind : true,
+    type   : 'loop',
+    perMove: 1,
+    focus  : 'center',
+    autoplay: true,
+    gap    : '.5rem',
+  } );
+  
+  splide.mount();
+  
